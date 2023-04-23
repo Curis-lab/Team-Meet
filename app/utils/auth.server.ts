f7cdf7978c0154b418917eb5b5a270a8b8e0a9c5
Nyan Lin <mathnyanlin@gmail.com>
2023-04-23 16:57:12 +0630

    change primer server

--- app/utils/auth.server.ts
+++ app/utils/auth.server.ts
@@ -1,11 +1,11 @@
 import { json } from "@remix-run/node"
-import { prisma } from "./prisma.server"
+import { db } from "./prisma.server"
 import type { Registerform, loginForm } from "./types.server"
 import { createUser } from "./users.server"
 import bcrypt from 'bcryptjs';
 
 export const register = async(form: Registerform)=>{
-    const exist = await prisma.user.count({where:{email: form.email}})
+    const exist = await db.user.count({where:{email: form.email}})
     if(exist){
         return json(
             {error:'they alerady have an account'},
@@ -13,7 +13,6 @@ export const register = async(form: Registerform)=>{
         )
     }
 
-    //create user
     const newUser = await createUser(form)
     if(!newUser){
         return json(
@@ -30,7 +29,7 @@ export const register = async(form: Registerform)=>{
 }
 
 export const login = async (form: loginForm)=>{
-    const user = await prisma.user.findUnique({
+    const user = await db.user.findUnique({
         where: {email: form.email},
     })
     if(!user || !(await bcrypt.compare( form.password, user.password))){
