e4911c6e3027b45e02bc09ae50b4248a4c6da43a
Nyan Lin <mathnyanlin@gmail.com>
2023-05-03 07:38:06 +0630

    fixed profile

--- app/routes/home.profile.tsx
+++ app/routes/home.profile.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunction, json } from "@remix-run/node"
+import { ActionFunction, LoaderFunction, json } from "@remix-run/node"
 import { useLoaderData } from "@remix-run/react";
 import { useState } from "react";
 import { Formfield } from "~/components/form-field";
@@ -12,6 +12,11 @@ export const loader: LoaderFunction=async({request})=>{
     return json({user});
 }
 
+export const action: ActionFunction = async({request})=>{
+    const a = await getUser(request);
+    console.log(a);
+    return null
+}
 export default function ProfileModal(){
     const {user} = useLoaderData();
 
@@ -58,7 +63,7 @@ export default function ProfileModal(){
                         onChange={e=> handleInputChange(e,'department')}
                         />
                         <div className="w-full text-right mt-7">
-                            <button name="_action" className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 hover:-translate-y-2">
+                            <button name="_action" type="submit" value="save" className="rounded-xl bg-yellow-300 font-semibold text-blue-600 px-16 py-2 transition duration-300 hover:-translate-y-2">
                                 Save
                             </button>
                         </div>
